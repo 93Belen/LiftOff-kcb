33f6611a5d71370cce46e2fa3d95f460d55c25c8
Belen Garcia Martinez <93bgarciamartinez@gmail.com>
2023-01-28 07:03:32 -0600

    tweak business object that will be stored in redux

--- src/call-backend/getAllBusinesses.js
+++ src/call-backend/getAllBusinesses.js
@@ -111,13 +111,7 @@ let obj = {
             Immigrant: [],
             Asian: [],
             LGBTQIA: []
-        },
-        Woman: [],
-        Latino: [],
-        Black: [],
-        Immigrant: [],
-        LGBTQIA: [],
-        Asian: []
+        }
     },
     Linn: {
         all: {
@@ -341,13 +335,7 @@ let obj = {
             Immigrant: [],
             Asian: [],
             LGBTQIA: []
-        },
-        Woman: [],
-        Latino: [],
-        Black: [],
-        Immigrant: [],
-        LGBTQIA: [],
-        Asian: []
+        }
     },
     Leavenworth: {
         all: {
@@ -571,13 +559,7 @@ let obj = {
             Immigrant: [],
             Asian: [],
             LGBTQIA: []
-        },
-        Woman: [],
-        Latino: [],
-        Black: [],
-        Immigrant: [],
-        LGBTQIA: [],
-        Asian: []
+        }
     },
     Caldwell: {
         all: {
@@ -801,13 +783,7 @@ let obj = {
             Immigrant: [],
             Asian: [],
             LGBTQIA: []
-        },
-        Woman: [],
-        Latino: [],
-        Black: [],
-        Immigrant: [],
-        LGBTQIA: [],
-        Asian: []
+        }
     },
     Cass: {
         all: {
@@ -878,7 +854,6 @@ export const getAllBusinesses = async() => {
         const response = await fetch("http://localhost:8080/api/businesses");
         if(response.ok){
             const jsonResponse = response.json();
-            console.log(jsonResponse)
             return jsonResponse;
         }
         else {
@@ -892,57 +867,87 @@ export const getAllBusinesses = async() => {
 
 
 
-// Get business type into right section
+// Get business type into right sections that they fall in
 const getRightBusinessType = (str) => {
-    let food = [];
-    let coffee = [];
-    let clothing = [];
-    let homegoods = [];
-    let others = [];
+    let food = ['food', 'restaurant'];
+    let coffee = ['coffee'];
+    let clothing = ['clothing', 'fashion'];
+    let homegoods = ['home', 'homegoods'];
     let arr = str.split(' ');
     let type = [];
-    if(arr.includes('Food') || arr.includes('food')){
-        type.push('food')
-    }
-    else if(arr.includes('Coffee') || arr.includes('coffee')){
-        type.push('coffee')
-    }
-    else if(arr.includes('Clothing') || arr.includes('clothing')){
-        type.push('clothing')
-    }
-    else if(arr.includes('Home') || arr.includes('home')){
-        type.push('homegoods')
-    }
-    else {
-        type.push('others')
-    }
-    return type[0]
+    arr.forEach(word => {
+        if(food.includes(word.toLowerCase())){
+            type.push('food')
+        }
+        else if(coffee.includes(word.toLowerCase())){
+            type.push('coffee')
+        }
+        else if(clothing.includes(word.toLowerCase())){
+            type.push('clothing')
+        }
+        else if(homegoods.includes(word.toLowerCase())){
+            type.push('homegoods')
+        }
+        else {
+            type.push('others')
+        }
+    })
+    // return array of business types
+    return type
 
 }
 
 
 
-// Add array into object
-export const tranformIntoObj = (arr) => {
+// Populate object witn businesses ids
+const tranformIntoObj = (arr) => {
+    // Loop throw businesses aaray
     arr.forEach(business => {
+        // Get business types selection based in what owner wrote as business type
         business.businessType.name = getRightBusinessType(business.businessType.name)
+        // If the business is not already in the object
         if(!obj.all.all.all.includes(business.id)){
+            // Push the business id to all needed sections
             obj.all.all.all.push(business.id);
             obj[business.businessLocation.county].all.all.push(business.id);
-            obj[business.businessLocation.county][business.businessType.name].all.push(business.id);
-            obj.all[business.businessType.name].all.push(business.id);
+            // The same business might fall in different business type categories, so add it to all those categories
+            for(let i = 0; i < business.businessType.name.length; i++){
+                if(!obj[business.businessLocation.county][business.businessType.name[i]].all.includes(business.id)){
+                    obj[business.businessLocation.county][business.businessType.name[i]].all.push(business.id);
+                }
+                if(!obj.all[business.businessType.name[i]].all.includes(business.id)){
+                    obj.all[business.businessType.name[i]].all.push(business.id);
+                }
+            }
+            // The owner might be part of many different minority group, so for each one:
             for(const owner of business.ownerTypes){
                 obj[business.businessLocation.county].all[owner.name].push(business.id);
                 obj.all.all[owner.name].push(business.id);
-                obj.all[business.businessType.name][owner.name].push(business.id);
-                obj[business.businessLocation.county][business.businessType.name][owner.name].push(business.id);
+                for(let i = 0; i < business.businessType.name.length; i++){
+                    if(!obj.all[business.businessType.name[i]][owner.name].includes(business.id)){
+                        obj.all[business.businessType.name[i]][owner.name].push(business.id);
+                    }
+                    if(!obj[business.businessLocation.county][business.businessType.name[i]][owner.name].includes(business.id)){
+                        obj[business.businessLocation.county][business.businessType.name[i]][owner.name].push(business.id);
+                    }
+                    
+                }
+                
             }   
         }       
 
     })
-    console.log(obj);
+    // return populated object
     return obj;
 }
+
+
+    // fetch all businesses here and store them in redux
+    export const getBusinessFromBackEnd = () => {        
+        const response = getAllBusinesses().then(resp => tranformIntoObj(resp));
+        return response
+    
+    }
 
 
 
