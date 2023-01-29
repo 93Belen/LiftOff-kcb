63330ac5e45a753f9dd63c5addea18170de7cb42
Belen Garcia Martinez <93bgarciamartinez@gmail.com>
2023-01-29 08:42:51 -0600

    function to get business by id, done, returns cors error

--- src/components/Buttons/Edit.js
+++ src/components/Buttons/Edit.js
@@ -2,12 +2,34 @@ import { Button } from "react-bootstrap"
 import { useDispatch } from "react-redux";
 import './Buttons.css';
 
-export const Edit = () => {
+export const Edit = (props) => {
     const dispatch = useDispatch();
+    let id = props.id;
+    let jwt = localStorage.getItem('jwt');
 
 
     const onClick = () => {
         dispatch({type: 'editingAdding/changeState', payload: 'editing'})
+        const getBusinessToEdit = async() => {
+            try {
+                const response = await fetch(`http://localhost:8080/api/businesses/${id}`, {
+                    headers: {
+                        "Content-type": "application/json",
+                        "Cache-Control": "no-cache",
+                        "Authorization": "Bearer " + jwt
+                    }
+                });
+                    if(response.ok){
+                        const jsonResponse = response.json();
+                        return jsonResponse;
+                    }
+                } catch (e) {
+                console.log(e)
+            }
+        }
+        getBusinessToEdit().then(response => {
+            console.log(response);
+        })
     }
     
     return (
