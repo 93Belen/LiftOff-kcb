76abccd8e8fd3e41d16ca151a6811472da103a06
Belen Garcia Martinez <93bgarciamartinez@gmail.com>
2023-02-10 06:34:40 -0600

    added test description

--- src/tests/getAllBusinesses.test.js
+++ src/tests/getAllBusinesses.test.js
@@ -1,2 +1,16 @@
 import { tranformIntoObj } from "../call-backend/getAllBusinesses";
 
+
+test("Adds different food business to food type", () => {
+
+})
+test("Adds different clothing business to clothing type", () => {
+    
+})
+test("Adds different home goods business to home goods type", () => {
+    
+})
+test("Adds not included business types to others", () => {
+    
+})
+
