7c1a1ebf894e137b7b5b0adf50717324479bcc1e
Belen Garcia Martinez <belengm.dev@gmail.com>
2023-02-22 06:05:46 -0600

    passes 12 tests

--- src/tests/displayCards.test.js
+++ src/tests/displayCards.test.js
@@ -71,3 +71,17 @@ test("Returns all business of selected owner type, county, and business type whe
     const actual = displayCards(locationFilters, ownerTypeFilters, businessTypeFilters)
     expect(actual).toEqual([1, 2, 3, 4])
 })
+test("Returns all business of selected owner type, county, and business type when all filters are selected with multiple options and one option is not present in any business", () => {
+    let locationFilters = ['bates', 'jackson', 'platte', 'almeria'];
+    let ownerTypeFilters = ['asian', 'woman'];
+    let businessTypeFilters = ['food', 'coffee'];
+    const actual = displayCards(locationFilters, ownerTypeFilters, businessTypeFilters)
+    expect(actual).toEqual([1, 2, 3, 4])
+})
+test("Returns all business of selected owner type, county, and business type when all filters are selected with multiple options and one business fullfil multiple options", () => {
+    let locationFilters = ['bates', 'jackson', 'platte'];
+    let ownerTypeFilters = ['asian', 'woman', 'lgbtqia'];
+    let businessTypeFilters = ['food', 'coffee'];
+    const actual = displayCards(locationFilters, ownerTypeFilters, businessTypeFilters)
+    expect(actual).toEqual([1, 2, 3, 4])
+})
